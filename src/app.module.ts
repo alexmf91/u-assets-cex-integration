417f573009cc70e88b4c7b41b44ceabee5549f4a
alexmf91 <alexmunoz91@gmail.com>
2025-06-28 21:55:14 +0200

    feat: update app module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,35 @@
-import { Module } from '@nestjs/common'
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common'
 import { ConfigModule } from '@nestjs/config'
+
 import { AppController } from './app.controller'
 import { AppService } from './app.service'
+import { LoggerMiddleware } from './common/middleware'
+import { AssetsModule } from './modules/assets'
+import { AuthModule } from './modules/auth'
+import { DatabaseModule } from './modules/database'
+import { OrdersModule } from './modules/orders'
+import { QuotesModule } from './modules/quotes'
+import { RedisModule } from './modules/redis'
+import { UserModule } from './modules/users'
 
 @Module({
 	imports: [
 		ConfigModule.forRoot({
 			isGlobal: true
-		})
+		}),
+		DatabaseModule,
+		RedisModule,
+		AuthModule,
+		AssetsModule,
+		UserModule,
+		OrdersModule,
+		QuotesModule
 	],
 	controllers: [AppController],
 	providers: [AppService]
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(LoggerMiddleware).forRoutes('')
+	}
+}
